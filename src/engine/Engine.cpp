39dba59c85b7287adaa48e3b04b643baf8a03b0b
Vladimir Sudzilovskiy <39033756+vlasud@users.noreply.github.com>
2025-07-25 15:50:10 -0700

    Create Engine.cpp

---
+++ src/engine/Engine.cpp
@@ -0,0 +1,13 @@
+#include "graviton/graviton.h"
+
+namespace graviton {
+
+void Engine::initialize() {
+    // TODO: Add engine initialization code here
+}
+
+void Engine::shutdown() {
+    // TODO: Add engine shutdown/cleanup code here
+}
+
+} // namespace graviton
